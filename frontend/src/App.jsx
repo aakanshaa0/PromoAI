751f383e52393c7842ee63a4f7147aa6ca3e288c
aakanshaa0 <aakanshapande23@gmail.com>
2025-08-17 21:44:08 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import { CssBaseline } from '@mui/material'
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './pages/Home'
 import Promote from './pages/Promote'
 import Dashboard from './pages/Dashboard'
-import { Analytics } from "@vercel/analytics"
 
 function App() {
   return (
@@ -13,9 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/promote" element={<Promote />} />
         <Route path="/dashboard" element={<Dashboard />} />
-
       </Routes>
-      <Analytics /> 
+      <Analytics />
     </>
   )
 }
